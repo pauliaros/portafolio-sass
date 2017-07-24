13f672b19ae1e65bd7433b94081db4f0ae0ebdb6
Paulina Aros <aros.pau@gmail.com>
2017-07-24 12:52:40 -0400

    estilos seccion plan casi listo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 /*funcion carrusel*/
 
 $('.carousel.carousel-slider').carousel({fullWidth: true});
+
+
+$(document).ready(function() {
+$(window).scroll(function() {
+        $('#nav-porta').toggleClass('navbar', $(this).scrollTop() > $('#nav-porta').height());
+      });
+});
